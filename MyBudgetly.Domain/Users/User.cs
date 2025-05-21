f8fb7509898d3c148fcfa227b218c7325c705014
Julia Stetsenko <julia.nsd.06@gmail.com>
2025-05-21 21:48:55 +0300

    Feat: Make user's primary email immutable and add backup email

--- MyBudgetly.Domain/Users/User.cs
+++ MyBudgetly.Domain/Users/User.cs
@@ -6,9 +6,10 @@ public class User : BaseEntity
 {
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public string Email { get; } = null!;
+    public string? BackupEmail { get; set; } = null!;
 
-    private User() 
+    private User()
     { }
 
     public User(string firstName, string lastName, string email)
@@ -22,6 +23,12 @@ public class User : BaseEntity
     {
         FirstName = firstName;
         LastName = lastName;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateBackupEmail(string? backupEmail) 
+    {
+        BackupEmail = backupEmail;
         UpdatedAt = DateTime.UtcNow;
     }
 }
