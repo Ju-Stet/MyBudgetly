afb51c4c88af6ce822b7089b1d60062c9f8e678f
Julia Stetsenko <julia.nsd.06@gmail.com>
2025-05-28 21:56:24 +0300

    Feat: Add Swagger configuration

--- MyBudgetly.API/Program.cs
+++ MyBudgetly.API/Program.cs
@@ -15,17 +15,35 @@ internal class Program
         builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
         builder.Services.AddScoped(provider => provider.GetRequiredService<IApplicationDbContext>());
         builder.Services.AddInfrastructure();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+                Title = "MyBudgetly API",
+                Version = "v1",
+                Description = "A simple API for managing personal budgets",
+                Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                {
+                    Name = "Julia Stetsenko",
+                    Email = "jul.stetsenko@gmail.com",
+                    Url = new Uri("https://github.com/Ju-Stet/MyBudgetly")
+                }
+            });
+        });
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-
-        app.Run();
-    }
 }
