20d8e01b4d8f847ce2fd8d5b74f64336b58cb4e8
Julia Stetsenko <julia.nsd.06@gmail.com>
2025-07-11 22:06:13 +0300

    Test: Add unit tests for DeleteUserCommand

---
+++ tests/MyBudgetly.Tests/Users/Commands/DeleteUserCommandTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Moq;
+using MyBudgetly.Application.Users.Commands;
+using MyBudgetly.Domain.Common.Exceptions;
+using MyBudgetly.Domain.Users;
+
+namespace MyBudgetly.Tests.Users.Commands;
+
+public class DeleteUserCommandTests
+{
+    private readonly Mock<IUserRepository> _userRepositoryMock = new();
+    private readonly DeleteUserCommand.Handler _handler;
+
+    public DeleteUserCommandTests()
+    {
+        _handler = new DeleteUserCommand.Handler(_userRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Delete_User_When_User_Exists()
+    {
+        // Arrange
+        var existingUser = new User("natasha@shield.gov");
+
+        _userRepositoryMock
+            .Setup(r => r.GetByIdAsync(existingUser.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(existingUser);
+
+        var command = new DeleteUserCommand.Message { UserId = existingUser.Id };
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().BeTrue();
+        _userRepositoryMock.Verify(r => r.RemoveAsync(existingUser.Id, It.IsAny<CancellationToken>()), Times.Once);
+        _userRepositoryMock.Verify(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Throw_When_User_Not_Found()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+
+        _userRepositoryMock
+            .Setup(r => r.GetByIdAsync(userId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((User?)null!);
+
+        var command = new DeleteUserCommand.Message { UserId = userId };
+
+        // Act
+        var act = () => _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        await act.Should()
+            .ThrowAsync<ExceptionWithStatusCode>()
+            .WithMessage($"*{userId}*not found*");
+
+        _userRepositoryMock.Verify(r => r.RemoveAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Never);
+        _userRepositoryMock.Verify(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+}
